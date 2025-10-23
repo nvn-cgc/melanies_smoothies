1b6354c8884600aee88935eef29ab2b15fff4b9b
Naveen Kumar <69457880+nvn-cgc@users.noreply.github.com>
2025-10-23 22:06:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie : cup_with_straw:")
 st.write(
